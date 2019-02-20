a8123fd8179c3e085cff4bdf5a89b1411c361b2c
darcops <diego.lopez.arcos@gmail.com>
2019-02-20 17:04:07 -0600

    creating assigment view

--- public/javascripts/controllers/devices.js
+++ public/javascripts/controllers/devices.js
@@ -4,6 +4,8 @@ app.controller('customerController', function($scope, $http, $window ,DTOptionsB
     var baseUrl = "http://localhost:8088/api/v1"
     var config = {};
     var provider_id = $window.location.href.split("/")[4]
+    $scope.selectedDevices = [];
+    $scope.showModal = false;
 
     $scope.dataTableOpt = {
       dom: 'Bfrtip',
@@ -21,6 +23,20 @@ app.controller('customerController', function($scope, $http, $window ,DTOptionsB
 
 
     loadDevices()
+
+    $scope.sm = function() {
+      $scope.showModal = !$scope.showModal;
+    }
+
+    $scope.clickedItem = function(device_id) {
+      if ($scope.selectedDevices.indexOf(device_id) < 0) {
+        $scope.selectedDevices.push(device_id);
+      } else {
+        $scope.selectedDevices.splice($scope.selectedDevices.indexOf(device_id),1)
+      }
+      console.log("clicked", device_id)
+      console.log("items", $scope.selectedDevices)
+    }
 
 
     $scope.saveDevice = function() {
@@ -54,65 +70,8 @@ app.controller('customerController', function($scope, $http, $window ,DTOptionsB
     }
 });
 
-//
-// var app = angular.module('app', [])
-//   app.controller('Devices',  function($scope, $http, $window) {
-//     console.log("in Devices controller");
-//     var baseUrl = "http://localhost:8088/api/v1"
-//     var config = {};
-//     var provider_id = $window.location.href.split("/")[4]
-//
-//
-//
-//
-//
-//     loadDevices = function() {
-//       $http.get(baseUrl+"/providers/"+provider_id+"/devices",config)
-//         .then(function(response) {
-//           $scope.devices = response.data;
-//           console.log("RESPUESTA DE DEVICES", $scope.providers)
-//       });
-//     }
-//
-//     $scope.saveDevice = function() {
-//       var data = {
-//         "provider_id": parseInt(provider_id),
-//         "imei": $scope.imei.toString(),
-//         "mpn": $scope.mpn,
-//         "mpn": $scope.name,
-//         "ubication": $scope.ubication,
-//         "admission_date": $scope.admission_date,
-//       }
-//       console.log("add provider data", data)
-//       $http.post(baseUrl+"/providers/"+provider_id+"/devices", data, config)
-//                .success(function (data, status, headers, config) {
-//                  swal({
-//                    type: 'success',
-//                    title: 'Device Created',
-//                    showConfirmButton: false,
-//                    timer: 1500
-//                  })
-//                  loadDevices();
-//
-//                })
-//                .error(function (data, status, header, config) {
-//                  swal({
-//                    type: 'warning',
-//                    title: 'An error ocurred',
-//                    showConfirmButton: false,
-//                    timer: 2500
-//                  })
-//                });
-//     }
-//
-//     loadDevices();
-//
-//   });
 
-
-
-
-    app.controller('layout', function($scope, $http, $window) {
+app.controller('layout', function($scope, $http, $window) {
       if(localStorage.getItem("token") === null)
       $window.location.href = "/login"
 
@@ -122,7 +81,7 @@ app.controller('customerController', function($scope, $http, $window ,DTOptionsB
       }
       });
 
-  app.controller('layout_user', function($scope, $http, $window) {
+app.controller('layout_user', function($scope, $http, $window) {
         $scope.userName = localStorage.getItem("user_name");
         $scope.userEmail = localStorage.getItem("user_email");
     });
