5c33e18b059e75757d150118a86dc446dfd6c0f2
darcops <diego.lopez.arcos@gmail.com>
2019-02-21 10:08:10 -0600

    csv uploaded

--- public/javascripts/controllers/devices.js
+++ public/javascripts/controllers/devices.js
@@ -1,9 +1,26 @@
 var app = angular.module('customerApp', ['datatables']);
+
+app.directive('ngFiles', ['$parse', function ($parse) {
+
+            function fn_link(scope, element, attrs) {
+                var onChange = $parse(attrs.ngFiles);
+                element.on('change', function (event) {
+                    onChange(scope, { $files: event.target.files });
+                });
+            };
+
+            return {
+                link: fn_link
+            }
+} ])
+
+
 app.controller('customerController', function($scope, $http, $window ,DTOptionsBuilder){
 
     var baseUrl = "http://localhost:8088/api/v1"
     var config = {};
     var provider_id = $window.location.href.split("/")[4]
+    var formdata = new FormData();
     $scope.selectedDevices = [];
     $scope.showModal = false;
 
@@ -19,9 +36,6 @@ app.controller('customerController', function($scope, $http, $window ,DTOptionsB
         $scope.devices = data;
       });
     }
-
-
-
     loadDevices()
 
     $scope.sm = function() {
@@ -68,6 +82,37 @@ app.controller('customerController', function($scope, $http, $window ,DTOptionsB
                  })
                });
     }
+
+    $scope.getTheFiles = function ($files) {
+              console.log("QUE PEDO selc", $files)
+               angular.forEach($files, function (value, key) {
+                   formdata.append("file", value);
+               });
+    };
+
+    $scope.uploadFiles = function () {
+
+                var request = {
+                    method: 'POST',
+                    url: baseUrl+"/providers/"+provider_id+"/devices/upload_file",
+                    data: formdata,
+                    headers: {
+                        'Content-Type': undefined
+                    }
+                };
+
+                // SEND THE FILES.
+                $http(request)
+                    .success(function (d) {
+                        alert(d);
+                        loadDevices();
+                        console.log("hecho", d)
+                    })
+                    .error(function (e) {
+                      console.log("err", e)
+                    });
+    }
+
 });
 
 
